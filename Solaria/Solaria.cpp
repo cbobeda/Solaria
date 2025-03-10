4e1ba0acbadb83e5e2b4c9d2ca3cb2a0bcecdb09
cbobeda <cbobeda@gaming.tech>
2025-03-10 12:00:52 +0100

    merge

--- Solaria/Solaria.cpp
+++ Solaria/Solaria.cpp
@@ -12,7 +12,7 @@ int main(int argc, char* argv[])
 	while (window.isOpen())
 	{
         while (window.isOpen())
-        {
+        { 
             window.clear();
             window.display();
         }
