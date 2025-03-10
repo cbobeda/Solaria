f768258c8828a1d60793c77de0268232fc8b557d
cbobeda <cbobeda@gaming.tech>
2025-03-10 13:38:10 +0100

    Ennemi first version

---
+++ Solaria/Ennemi.h
@@ -0,0 +1,18 @@
+﻿#pragma once
+#include "Entity.h"
+class Ennemi : public Entity
+{
+
+private:
+    int hp = 3, i = 0;
+    float speed = 50;
+    sf::Texture texture;
+    sf::Sprite sprite;
+    sf::Vector2f pos;
+    sf::Vector2f positionP[2];
+public:
+    Ennemi(sf::Vector2f pos, int patrolRange);
+    void update(float deltatime) override;
+    void draw(RenderWindow& window) override;
+    ~Ennemi();
+};
--- Solaria/Solaria.cpp
+++ Solaria/Solaria.cpp
@@ -1,20 +1,23 @@
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include "Ennemi.h"
 
 using namespace sf;
 using namespace std;
 
 
+Ennemi test({200.f,200.f}, 200);
+
 int main(int argc, char* argv[])
 {
     
-    RenderWindow window(VideoMode(1500, 500), "Solaria");
+    RenderWindow window(VideoMode(1920, 1080), "Solaria");
 	while (window.isOpen())
 	{
-        while (window.isOpen())
-        { 
-            window.clear();
-            window.display();
-        }
+		test.update(0.01);
+		window.clear();
+		test.draw(window);
+		window.display();
+        
 	}
 }
